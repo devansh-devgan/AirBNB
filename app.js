bbfce59bf3f83a4e10dde53e6ac1218b367db997
Devansh Devgan <devddevgan123@gmail.com>
2025-03-12 22:51:32 +0530

    removing - console.log("Server is listening to port: 8080");

--- app.js
+++ app.js
@@ -123,5 +123,5 @@ app.use((err,req,res,next) => {
 })
 
 app.listen(8080, () => {
-    console.log("Server is listening to port: 8080");
+    // console.log("Server is listening to port: 8080");
 })
